ae9e8dda1faf1ad28bd07c08cbdd34e6faa2fc58
Z7X4Y2HQ <87140889+Z7X4Y2HQ@users.noreply.github.com>
2024-05-05 14:29:23 +0500

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static("dist"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello Niggers!</h1>");
+  res.send("<h1>Hello!</h1>");
 });
 app.get("/api/notes", (req, res) => {
   Note.find({}).then((notes) => {
@@ -18,27 +18,27 @@ app.get("/api/notes", (req, res) => {
   });
 });
 
-app.get("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const note = notes.find((n) => n.id === id);
-  if (note) {
-    res.json(note);
-  } else {
-    res.statusMessage = `note ${id} doesn't exist`;
-    res.status(404).end();
-  }
+app.get("/api/notes/:id", (req, res, next) => {
+  Note.findById(req.params.id)
+    .then((note) => {
+      if (note) {
+        res.json(note);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
-app.delete("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  notes = notes.filter((note) => note.id !== id);
-  res.status(204).end();
+app.delete("/api/notes/:id", (req, res, next) => {
+  Note.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
 });
-
-const generateID = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
 
 app.post("/api/notes", (req, res) => {
   const body = req.body;
@@ -49,16 +49,48 @@ app.post("/api/notes", (req, res) => {
     });
   }
 
-  const note = {
-    id: generateID(),
+  const note = new Note({
     content: body.content,
-    important: Boolean(body.important) || false,
-  };
-  notes = notes.concat(note);
+    important: body.important || false,
+  });
+
+  note.save().then((result) => {
+    res.json(result);
+  });
 
   console.log(note);
-  res.json(note);
 });
+
+app.put("/api/notes/:id", (req, res, next) => {
+  const body = req.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+    .then((updatedNote) => {
+      res.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "Unknown Endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+app.use(errorHandler);
 
 const port = process.env.PORT;
 app.listen(port, () => {
